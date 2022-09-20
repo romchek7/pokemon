687b460d8949939cfa86cf971fd9ece70c60d76a
romchek7 <romanpitulia@gmail.com>
2022-09-20 18:09:47 +0300

    useSelector with types, own hook

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import {useSelector} from "react-redux";
+import {useTypedSelector} from "../../hooks/useTypedSelector";
 
 const PokemonList: React.FC = () => {
-    const state = useSelector(state => state)
+    const {pokemons, loading, error} = useTypedSelector(state => state.pokemon)
 
     console.log(state)
 
--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -4,3 +4,5 @@ import { pokemonReducer } from "./pokemonReducer";
 export const rootReducer = combineReducers({
     pokemon: pokemonReducer
 })
+
+export type rootState = ReturnType<typeof rootReducer>
